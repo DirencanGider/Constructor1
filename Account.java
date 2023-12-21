6383ef3f910ab0be2aed26491488420e288594fb
DirencanGider <direncangider@hotmail.com>
2023-12-22 00:21:32 +0300

    Commit Message 22.12.23 00:21

---
+++ Account.java
@@ -0,0 +1,95 @@
+public class Account {
+
+    //We will create Account class' features first.
+
+    private String accountNo;
+    private double balance; //Bakiye(TR)
+    private String name;
+
+    private String email;
+
+    private String phoneNo;
+
+    public Account() {
+
+        //System.out.println("Constructor that we made");
+
+        this.accountNo =
+
+
+    }
+    public Account(String accountNo, double balance, String name, String email, String phoneNo) {
+
+        this.accountNo = accountNo;
+        this.balance = balance;
+        this.name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+
+    }
+
+    public void depositMoney (double amount) { //Para yatirma islemi metodu yazdik. (TR)
+
+        balance += amount;
+
+        System.out.println("Your New Balance: " + balance);
+
+    }
+
+    public void withdrawalls (double amount) { //Para cekme islemi metodu yazdik. (TR)
+
+        if (amount > 1500) {
+            System.out.println("The limit of withdrawals cannot be over 1500...");
+        }
+        if (balance - amount < 0) {
+            System.out.println("There is no enough money in your balance. Your balance: " + balance);
+        }
+        else {
+            balance -= amount;
+            System.out.println("Your New Balance: " + balance);
+        }
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+
+}
---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        /*
+        //Account account1 = new Account();
+
+        Account account2 = new Account("123456", 1000.0, "Direncan Gider","direncan@hotmail.com", "5551112233");
+
+        //account2.depositMoney(500.0);
+
+        //account2.withdrawalls(700.0);
+
+        account2.withdrawalls(2000.0);
+        */
+
+
+
+
+
+    }
+
+}
